93655887169ff7df1cf4eb365989d9a00e4f2067
Rewaa-Zafer <Rewzazafer1@gmail.com>
2021-11-01 20:11:23 +0200

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //sheet1 ch5
+
+
+
+	   /* Q17
+        for (int i=1;i<=7;i++)
+         {
+            for(int j=8-i;j>=1;j--)
+            {System.out.print(j);}
+            for(int k=2;k<=8-i;k++)
+            {System.out.print(k);}
+            System.out.println();
+         }*/
+
+
+
+
+        /*Q19
+        for(int i=1;i<=8;i++)
+        {for(int j=1;j<=38-4*i;j++){System.out.print(" ");}
+          for (int p=0;p<=(i-1);p++)
+            {if (p==0||p==1||p==2){System.out.print((int) Math.pow(3,p));
+                System.out.print(" "+" "+" ");}
+            else if(p==3||p==4){System.out.print((int) Math.pow(3,p));
+                System.out.print(" "+" ");}
+            else if(p==5||p==6){System.out.print((int) Math.pow(3,p));
+                System.out.print(" ");}
+            else {System.out.print((int) Math.pow(3,p));
+                System.out.print("");}}
+          for (int k=(i-2);k>=0;k--)
+            {if (k==0||k==1||k==2){System.out.print((int) Math.pow(3,k));
+                System.out.print(" "+" "+" ");}
+            else if(k==3||k==4){System.out.print((int) Math.pow(3,k));
+                System.out.print(" "+" ");}
+            else if(k==5||k==6){System.out.print((int) Math.pow(3,k));
+                System.out.print(" ");}
+            else {System.out.print((int) Math.pow(3,k));
+                System.out.print("");}}System.out.println();}*/
+
+
+
+        /*Q37
+        int x,z=0,a=0,b=1,c;
+       // Scanner y = new Scanner(System.in);x= y.nextInt();
+        x = Integer.parseInt(JOptionPane.showInputDialog("please, enter decimal number "));
+        System.out.print("the pinary num of"+" "+x+" ");
+        while(x!=0)
+        { z=x%2;
+          x=x/2;
+          a=a+(z*b);
+          b=b*10;   }
+          System.out.print("is"+" "+a);*/
+
+        }}
